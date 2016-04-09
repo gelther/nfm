8c9c2eba58d132d6b0bcbccc2df0dda6dcb99376
gelther <lpw.sormano@gmail.com>
2016-04-09 20:42:32 +0200

    Code consistency changes for includes/Config/Example.php

--- includes/Config/Example.php
+++ includes/Config/Example.php
@@ -16,13 +16,13 @@ return apply_filters( 'ninja_forms_example_settings', array(
      */
 
     'setting_name_here' => array(
-        'name' => 'setting_name_here',
-        'type' => 'textbox', // 'textarea', 'number', 'toggle', etc
-        'label' => __( 'Label Here', 'ninja-forms'),
-        'width' => 'one-half', // 'full', 'one-half', 'one-third'
-        'group' => 'primary', // 'primary', 'restrictions', 'advanced'
-        'value' => '',
-        'help' => __( 'Help Text Here', 'ninja-forms' ),
+        'name'           => 'setting_name_here',
+        'type'           => 'textbox', // 'textarea', 'number', 'toggle', etc
+        'label'          => __( 'Label Here', 'ninja-forms'),
+        'width'          => 'one-half', // 'full', 'one-half', 'one-third'
+        'group'          => 'primary', // 'primary', 'restrictions', 'advanced'
+        'value'          => '',
+        'help'           => __( 'Help Text Here', 'ninja-forms' ),
         'use_merge_tags' => TRUE, // TRUE or FALSE
     ),
 
