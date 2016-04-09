7ec8779ac63915a61611eeea837f00d7e75ce604
gelther <lpw.sormano@gmail.com>
2016-04-09 20:42:25 +0200

    Code consistency changes for includes/Config/ActionEmailSettings.php

--- includes/Config/ActionEmailSettings.php
+++ includes/Config/ActionEmailSettings.php
@@ -12,14 +12,14 @@ return apply_filters( 'ninja_forms_action_email_settings', array(
      * To
      */
 
-    'to' => array(
-        'name' => 'to',
-        'type' => 'textbox',
-        'group' => 'primary',
-        'label' => __( 'To', 'ninja-forms' ),
-        'placeholder' => __( 'Email address or search for a field', 'ninja-forms' ),
-        'value' => '',
-        'width' => 'full',
+    'to'            => array(
+        'name'           => 'to',
+        'type'           => 'textbox',
+        'group'          => 'primary',
+        'label'          => __( 'To', 'ninja-forms' ),
+        'placeholder'    => __( 'Email address or search for a field', 'ninja-forms' ),
+        'value'          => '',
+        'width'          => 'full',
         'use_merge_tags' => TRUE,
     ),
 
@@ -28,13 +28,13 @@ return apply_filters( 'ninja_forms_action_email_settings', array(
      */
 
     'email_subject' => array(
-        'name' => 'email_subject',
-        'type' => 'textbox',
-        'group' => 'primary',
-        'label' => __( 'Subject', 'ninja-forms' ),
-        'placeholder' => __( 'Subject Text or seach for a field', 'ninja-forms' ),
-        'value' => '',
-        'width' => 'full',
+        'name'           => 'email_subject',
+        'type'           => 'textbox',
+        'group'          => 'primary',
+        'label'          => __( 'Subject', 'ninja-forms' ),
+        'placeholder'    => __( 'Subject Text or seach for a field', 'ninja-forms' ),
+        'value'          => '',
+        'width'          => 'full',
         'use_merge_tags' => TRUE,
     ),
 
@@ -43,13 +43,13 @@ return apply_filters( 'ninja_forms_action_email_settings', array(
      */
 
     'email_message' => array(
-        'name' => 'email_message',
-        'type' => 'rte',
-        'group' => 'primary',
-        'label' => __( 'Email Message', 'ninja-forms' ),
-        'placeholder' => '',
-        'value' => '',
-        'width' => 'full',
+        'name'           => 'email_message',
+        'type'           => 'rte',
+        'group'          => 'primary',
+        'label'          => __( 'Email Message', 'ninja-forms' ),
+        'placeholder'    => '',
+        'value'          => '',
+        'width'          => 'full',
         'use_merge_tags' => TRUE,
     ),
 
@@ -63,14 +63,14 @@ return apply_filters( 'ninja_forms_action_email_settings', array(
      * From Name
      */
 
-    'from_name' => array(
-        'name' => 'from_name',
-        'type' => 'textbox',
-        'group' => 'advanced',
-        'label' => __( 'From Name', 'ninja-forms' ),
-        'placeholder' => __( 'Name or fields', 'ninja-forms' ),
-        'value' => '',
-        'width' => 'one-half',
+    'from_name'     => array(
+        'name'           => 'from_name',
+        'type'           => 'textbox',
+        'group'          => 'advanced',
+        'label'          => __( 'From Name', 'ninja-forms' ),
+        'placeholder'    => __( 'Name or fields', 'ninja-forms' ),
+        'value'          => '',
+        'width'          => 'one-half',
         'use_merge_tags' => TRUE,
     ),
 
@@ -78,13 +78,13 @@ return apply_filters( 'ninja_forms_action_email_settings', array(
      * From Address
      */
 
-    'from_address' => array(
-        'name' => 'from_address',
-        'type' => 'textbox',
-        'group' => 'advanced',
-        'label' => __( 'From Address', 'ninja-forms' ),
-        'placeholder' => __( 'One email address or field', 'ninja-forms' ),
-        'value' => '',
+    'from_address'  => array(
+        'name'           => 'from_address',
+        'type'           => 'textbox',
+        'group'          => 'advanced',
+        'label'          => __( 'From Address', 'ninja-forms' ),
+        'placeholder'    => __( 'One email address or field', 'ninja-forms' ),
+        'value'          => '',
         'use_merge_tags' => TRUE,
     ),
 
@@ -92,13 +92,13 @@ return apply_filters( 'ninja_forms_action_email_settings', array(
      * Reply To
      */
 
-    'reply_to' => array(
-        'name' => 'reply_to',
-        'type' => 'textbox',
-        'group' => 'advanced',
-        'label' => __( 'Reply To', 'ninja-forms' ),
-        'placeholder' => '',
-        'value' => '',
+    'reply_to'      => array(
+        'name'           => 'reply_to',
+        'type'           => 'textbox',
+        'group'          => 'advanced',
+        'label'          => __( 'Reply To', 'ninja-forms' ),
+        'placeholder'    => '',
+        'value'          => '',
         'use_merge_tags' => TRUE,
     ),
 
@@ -106,16 +106,16 @@ return apply_filters( 'ninja_forms_action_email_settings', array(
      * Format
      */
 
-    'email_format' => array(
-        'name' => 'email_format',
-        'type' => 'select',
+    'email_format'  => array(
+        'name'    => 'email_format',
+        'type'    => 'select',
             'options' => array(
                 array( 'label' => __( 'HTML', 'ninja-forms' ), 'value' => 'html' ),
                 array( 'label' => __( 'Plain Text', 'ninja-forms' ), 'value' => 'plain' )
             ),
-        'group' => 'advanced',
-        'label' => __( 'Format', 'ninja-forms' ),
-        'value' => 'html',
+        'group'   => 'advanced',
+        'label'   => __( 'Format', 'ninja-forms' ),
+        'value'   => 'html',
         
     ),
 
@@ -123,13 +123,13 @@ return apply_filters( 'ninja_forms_action_email_settings', array(
      * Cc
      */
 
-    'cc' => array(
-        'name' => 'cc',
-        'type' => 'textbox',
-        'group' => 'advanced',
-        'label' => __( 'Cc', 'ninja-forms' ),
-        'placeholder' => '',
-        'value' => '',
+    'cc'            => array(
+        'name'           => 'cc',
+        'type'           => 'textbox',
+        'group'          => 'advanced',
+        'label'          => __( 'Cc', 'ninja-forms' ),
+        'placeholder'    => '',
+        'value'          => '',
         'use_merge_tags' => TRUE,
     ),
 
@@ -137,13 +137,13 @@ return apply_filters( 'ninja_forms_action_email_settings', array(
      * Bcc
      */
 
-    'bcc' => array(
-        'name' => 'bcc',
-        'type' => 'textbox',
-        'group' => 'advanced',
-        'label' => __( 'Bcc', 'ninja-forms' ),
-        'placeholder' => '',
-        'value' => '',
+    'bcc'           => array(
+        'name'           => 'bcc',
+        'type'           => 'textbox',
+        'group'          => 'advanced',
+        'label'          => __( 'Bcc', 'ninja-forms' ),
+        'placeholder'    => '',
+        'value'          => '',
         'use_merge_tags' => TRUE,
     ),
 
@@ -151,9 +151,9 @@ return apply_filters( 'ninja_forms_action_email_settings', array(
      * Attach CSV
      */
 
-    'attach_csv' => array(
-        'name' => 'attach_csv',
-        'type' => 'toggle',
+    'attach_csv'    => array(
+        'name'  => 'attach_csv',
+        'type'  => 'toggle',
         'group' => 'advanced',
         'label' => __( 'Attach CSV', 'ninja-forms' ),
     ),
