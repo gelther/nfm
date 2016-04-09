08d11969a21b00395f344ac03f9fadd4852809b2
gelther <lpw.sormano@gmail.com>
2016-04-09 20:42:52 +0200

    Code consistency changes for includes/Config/PluginSettingsDefaults.php

--- includes/Config/PluginSettingsDefaults.php
+++ includes/Config/PluginSettingsDefaults.php
@@ -2,14 +2,14 @@
 
 return apply_filters( 'ninja_forms_plugin_settings_defaults', array(
 
-    'date_format' => 'm/d/Y',
-    'currency_symbol' => '$',
+    'date_format'           => 'm/d/Y',
+    'currency_symbol'       => '$',
 
-    'recaptcha_site_key' => '',
-    'recaptcha_secret_key' => '',
-    'recaptcha_lang' => '',
+    'recaptcha_site_key'    => '',
+    'recaptcha_secret_key'  => '',
+    'recaptcha_lang'        => '',
 
-    'delete_on_uninstall' => 0,
+    'delete_on_uninstall'   => 0,
     'disable_admin_notices' => 0,
 
 ));
