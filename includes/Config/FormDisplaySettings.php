9a4195ae355635a7cf9c4497a27fd80b11447dd6
gelther <lpw.sormano@gmail.com>
2016-04-09 20:42:41 +0200

    Code consistency changes for includes/Config/FormDisplaySettings.php

--- includes/Config/FormDisplaySettings.php
+++ includes/Config/FormDisplaySettings.php
@@ -6,9 +6,9 @@ return apply_filters( 'ninja_forms_from_display_settings', array(
     * FORM TITLE
     */
 
-    'title' => array(
-        'name' => 'title',
-        'type' => 'textbox',
+    'title'          => array(
+        'name'  => 'title',
+        'type'  => 'textbox',
         'label' => __( 'Form Title', 'ninja-forms' ),
         'width' => 'full',
         'group' => 'primary',
@@ -20,9 +20,9 @@ return apply_filters( 'ninja_forms_from_display_settings', array(
     * SHOW FORM TITLE
     */
 
-    'show_title' => array(
-        'name' => 'show_title',
-        'type' => 'toggle',
+    'show_title'     => array(
+        'name'  => 'show_title',
+        'type'  => 'toggle',
         'label' => __( 'Display Form Title', 'ninja-forms' ),
         'width' => 'full',
         'group' => 'primary',
@@ -35,8 +35,8 @@ return apply_filters( 'ninja_forms_from_display_settings', array(
     */
 
     'clear_complete' => array(
-        'name' => 'clear_complete',
-        'type' => 'toggle',
+        'name'  => 'clear_complete',
+        'type'  => 'toggle',
         'label' => __( 'Clear successfully completed form?', 'ninja-forms' ),
         'width' => 'full',
         'group' => 'primary',
@@ -50,9 +50,9 @@ return apply_filters( 'ninja_forms_from_display_settings', array(
     * HIDE SUCCESSFULLY COMPLETED FORMS
     */
 
-    'hide_complete' => array(
-        'name' => 'hide_complete',
-        'type' => 'toggle',
+    'hide_complete'  => array(
+        'name'  => 'hide_complete',
+        'type'  => 'toggle',
         'label' => __( 'Hide successfully completed form?', 'ninja-forms' ),
         'width' => 'full',
         'group' => 'primary',
@@ -64,19 +64,19 @@ return apply_filters( 'ninja_forms_from_display_settings', array(
      * Currency
      */
 
-    'currency' => array(
-        'name' => 'currency',
-        'type' => 'select',
-        'label' => __( 'Currency', 'ninja-forms' ),
-        'group' => 'advanced',
-        'width' => 'full',
-        'options' => array(
+    'currency'       => array(
+        'name'           => 'currency',
+        'type'           => 'select',
+        'label'          => __( 'Currency', 'ninja-forms' ),
+        'group'          => 'advanced',
+        'width'          => 'full',
+        'options'        => array(
             array(
                 'label' => __( 'USD - $', 'ninja-forms' ),
                 'value' => 'usd'
             ),
         ),
-        'value' => 'usd',
+        'value'          => 'usd',
         'use_merge_tags' => FALSE
     ),
 
@@ -84,29 +84,29 @@ return apply_filters( 'ninja_forms_from_display_settings', array(
      * Classes
      */
 
-    'classes' => array(
-        'name' => 'classes',
-        'type' => 'fieldset',
-        'label' => __( 'Custom Class Names', 'ninja-forms' ),
-        'width' => 'full',
-        'group' => 'advanced',
+    'classes'        => array(
+        'name'     => 'classes',
+        'type'     => 'fieldset',
+        'label'    => __( 'Custom Class Names', 'ninja-forms' ),
+        'width'    => 'full',
+        'group'    => 'advanced',
         'settings' => array(
             array(
-                'name' => 'wrapper_class',
-                'type' => 'textbox',
-                'placeholder' => '',
-                'label' => __( 'Wrapper', 'ninja-forms' ),
-                'width' => 'one-half',
-                'value' => '',
+                'name'           => 'wrapper_class',
+                'type'           => 'textbox',
+                'placeholder'    => '',
+                'label'          => __( 'Wrapper', 'ninja-forms' ),
+                'width'          => 'one-half',
+                'value'          => '',
                 'use_merge_tags' => FALSE,
             ),
             array(
-                'name' => 'element_class',
-                'type' => 'textbox',
-                'label' => __( 'Element', 'ninja-forms' ),
-                'placeholder' => '',
-                'width' => 'one-half',
-                'value' => '',
+                'name'           => 'element_class',
+                'type'           => 'textbox',
+                'label'          => __( 'Element', 'ninja-forms' ),
+                'placeholder'    => '',
+                'width'          => 'one-half',
+                'value'          => '',
                 'use_merge_tags' => FALSE,
             ),
         ),
@@ -116,28 +116,28 @@ return apply_filters( 'ninja_forms_from_display_settings', array(
      * KEY
      */
 
-    'key' => array(
-        'name' => 'key',
-        'type' => 'textbox',
+    'key'            => array(
+        'name'  => 'key',
+        'type'  => 'textbox',
         'label' => __( 'Form Key', 'ninja-forms'),
         'width' => 'full',
         'group' => 'administration',
         'value' => '',
-        'help' => __( 'Programmatic name that can be used to reference this form.', 'ninja-forms' ),
+        'help'  => __( 'Programmatic name that can be used to reference this form.', 'ninja-forms' ),
     ),
 
     /*
      * ADD SUBMIT CHECKBOX
      */
 
-    'add_submit' => array(
-        'name' => 'add_submit',
-        'type' => 'toggle',
+    'add_submit'     => array(
+        'name'  => 'add_submit',
+        'type'  => 'toggle',
         'label' => __( 'Add Submit Button', 'ninja-forms'),
         'width' => 'full',
         'group' => '',
         'value' => 1,
-        'help' => __( 'We\'ve noticed that don\'t have a submit button on your form. We can add one for your automatically.', 'ninja-forms' ),
+        'help'  => __( 'We\'ve noticed that don\'t have a submit button on your form. We can add one for your automatically.', 'ninja-forms' ),
     ),
 
 
