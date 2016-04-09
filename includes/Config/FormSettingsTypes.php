e633c6b099d15da37746b31665cfddb7f6ea50a7
gelther <lpw.sormano@gmail.com>
2016-04-09 20:42:44 +0200

    Code consistency changes for includes/Config/FormSettingsTypes.php

--- includes/Config/FormSettingsTypes.php
+++ includes/Config/FormSettingsTypes.php
@@ -2,19 +2,19 @@
 
 return apply_filters( 'ninja_forms_from_settings_types', array(
 
-    'display' 			=> array(
-        'id' 			=> 'display',
-        'nicename' 		=> __( 'Display Settings', 'ninja-forms' ),
+    'display'      => array(
+        'id'       => 'display',
+        'nicename' => __( 'Display Settings', 'ninja-forms' ),
     ),
 
-    'restrictions' 		=> array(
-        'id' 			=> 'restrictions',
-        'nicename' 		=> __( 'Restrictions', 'ninja-forms' )
+    'restrictions' => array(
+        'id'       => 'restrictions',
+        'nicename' => __( 'Restrictions', 'ninja-forms' )
     ),
 
-    'calculations' 		=> array(
-    	'id' 			=> 'calculations',
-    	'nicename' 		=> __( 'Calculations', 'ninja-forms')
+    'calculations' => array(
+    	'id'       => 'calculations',
+    	'nicename' => __( 'Calculations', 'ninja-forms')
     ),
 
 ));
