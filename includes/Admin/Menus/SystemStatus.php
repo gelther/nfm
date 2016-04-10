6f0d20bce667223de9b4a47ea491dbc491cbdfda
gelther <lpw.sormano@gmail.com>
2016-04-10 11:47:16 +0200

    Code consistency changes for includes/Admin/Menus/SystemStatus.php

--- includes/Admin/Menus/SystemStatus.php
+++ includes/Admin/Menus/SystemStatus.php
@@ -95,7 +95,7 @@ final class NF_Admin_Menus_SystemStatus extends NF_Abstracts_Submenu
                 // link the plugin name to the plugin url if available
                 $plugin_name = $plugin_data['Name'];
                 if ( ! empty( $plugin_data['PluginURI'] ) ) {
-                    $plugin_name = '<a href="' . $plugin_data['PluginURI'] . '" title="' . __( 'Visit plugin homepage' , 'ninja-forms' ) . '">' . $plugin_name . '</a>';
+                    $plugin_name = '<a href="' . $plugin_data['PluginURI'] . '" title="' . __( 'Visit plugin homepage', 'ninja-forms' ) . '">' . $plugin_name . '</a>';
                 }
 
                 $all_plugins[] = $plugin_name . ' ' . __( 'by', 'ninja-forms' ) . ' ' . $plugin_data['Author'] . ' ' . __( 'version', 'ninja-forms' ) . ' ' . $plugin_data['Version'] . $version_string;
@@ -113,31 +113,31 @@ final class NF_Admin_Menus_SystemStatus extends NF_Abstracts_Submenu
 
         //Output array
         $environment = array(
-            __( 'Home URL','ninja-forms' ) => home_url(),
-            __( 'Site URL','ninja-forms' ) => site_url(),
-            __( 'Ninja Forms Version','ninja-forms' ) => esc_html( Ninja_Forms::VERSION ),
-            __( 'WP Version','ninja-forms' ) => get_bloginfo('version'),
-            __( 'WP Multisite Enabled','ninja-forms' ) => $multisite,
-            __( 'Web Server Info','ninja-forms' ) => esc_html( $_SERVER['SERVER_SOFTWARE'] ),
-            __( 'PHP Version','ninja-forms' ) => esc_html( phpversion() ),
+            __( 'Home URL', 'ninja-forms' ) => home_url(),
+            __( 'Site URL', 'ninja-forms' ) => site_url(),
+            __( 'Ninja Forms Version', 'ninja-forms' ) => esc_html( Ninja_Forms::VERSION ),
+            __( 'WP Version', 'ninja-forms' ) => get_bloginfo('version'),
+            __( 'WP Multisite Enabled', 'ninja-forms' ) => $multisite,
+            __( 'Web Server Info', 'ninja-forms' ) => esc_html( $_SERVER['SERVER_SOFTWARE'] ),
+            __( 'PHP Version', 'ninja-forms' ) => esc_html( phpversion() ),
             //TODO: Possibly Refactor with Ninja forms global $_db?
-            __( 'MySQL Version','ninja-forms' ) => $wpdb->db_version(),
-            __( 'PHP Locale','ninja-forms' ) =>  $data,
+            __( 'MySQL Version', 'ninja-forms' ) => $wpdb->db_version(),
+            __( 'PHP Locale', 'ninja-forms' ) =>  $data,
             //TODO: Possibly move the ninja_forms_letters_to_numbers function over.
-            __( 'WP Memory Limit','ninja-forms' ) => size_format( WP_MEMORY_LIMIT ),
+            __( 'WP Memory Limit', 'ninja-forms' ) => size_format( WP_MEMORY_LIMIT ),
             __( 'WP Debug Mode', 'ninja-forms' ) => $debug,
             __( 'WP Language', 'ninja-forms' ) => $lang,
-            __( 'WP Max Upload Size','ninja-forms' ) => size_format( wp_max_upload_size() ),
-            __('PHP Post Max Size','ninja-forms' ) => size_format( ini_get('post_max_size') ),
-            __('Max Input Nesting Level','ninja-forms' ) => ini_get('max_input_nesting_level'),
-            __('PHP Time Limit','ninja-forms' ) => ini_get('max_execution_time'),
-            __( 'PHP Max Input Vars','ninja-forms' ) => ini_get('max_input_vars'),
-            __( 'SUHOSIN Installed','ninja-forms' ) => $suhosin,
+            __( 'WP Max Upload Size', 'ninja-forms' ) => size_format( wp_max_upload_size() ),
+            __('PHP Post Max Size', 'ninja-forms' ) => size_format( ini_get('post_max_size') ),
+            __('Max Input Nesting Level', 'ninja-forms' ) => ini_get('max_input_nesting_level'),
+            __('PHP Time Limit', 'ninja-forms' ) => ini_get('max_execution_time'),
+            __( 'PHP Max Input Vars', 'ninja-forms' ) => ini_get('max_input_vars'),
+            __( 'SUHOSIN Installed', 'ninja-forms' ) => $suhosin,
             __( 'Server IP Address', 'ninja-forms' ) => $server_ip,
             __( 'Host Name', 'ninja-forms' ) => $host_name,
-            __( 'SMTP','ninja-forms' ) => ini_get('SMTP'),
-            __( 'smtp_port','ninja-forms' ) => ini_get('smtp_port'),
-            __( 'Default Timezone','ninja-forms' ) => $default_timezone,
+            __( 'SMTP', 'ninja-forms' ) => ini_get('SMTP'),
+            __( 'smtp_port', 'ninja-forms' ) => ini_get('smtp_port'),
+            __( 'Default Timezone', 'ninja-forms' ) => $default_timezone,
         );
 
         Ninja_Forms::template( 'admin-menu-system-status.html.php', compact( 'environment', 'site_wide_plugins' ) );
