0b50832d086d44036b5ae86f75a01cbbaa8fbcc0
gelther <lpw.sormano@gmail.com>
2016-04-10 15:58:09 +0200

    Code consistency changes for includes/Fields/FirstName.php

--- includes/Fields/FirstName.php
+++ includes/Fields/FirstName.php
@@ -27,13 +27,13 @@ class NF_Fields_FirstName extends NF_Abstracts_UserInfo
 
     public function filter_default_value( $default_value, $field_class, $settings )
     {
-        if( ! isset( $settings[ 'default_type' ] ) ||
+        if ( ! isset( $settings[ 'default_type' ] ) ||
             'user-meta' != $settings[ 'default_type' ] ||
             $this->_name != $field_class->get_name() ) return $default_value;
 
         $current_user = wp_get_current_user();
 
-        if( $current_user ) {
+        if ( $current_user ) {
             $default_value = $current_user->user_firstname;
         }
 
