0e0e447fa2ebc9166a3b04d9b0ab62b10cda7db1
gelther <lpw.sormano@gmail.com>
2016-04-10 16:12:56 +0200

    Code consistency changes for includes/MergeTags/Post.php

--- includes/MergeTags/Post.php
+++ includes/MergeTags/Post.php
@@ -7,27 +7,23 @@ final class NF_MergeTags_Post extends NF_Abstracts_MergeTags
 {
     protected $id = 'post';
 
-    public function __construct()
-    {
+    public function __construct() {
         parent::__construct();
         $this->title      = __( 'Post', 'ninja-forms' );
         $this->merge_tags = Ninja_Forms()->config( 'MergeTagsPost' );
     }
 
-    protected function post_id()
-    {
+    protected function post_id() {
         global $post;
         return ( is_object ( $post ) ) ? $post->ID : '';
     }
 
-    protected function post_title()
-    {
+    protected function post_title() {
         global $post;
         return ( is_object ( $post ) ) ? $post->post_title : '';
     }
 
-    protected function post_url()
-    {
+    protected function post_url() {
         global $post;
         return ( is_object ( $post ) ) ? get_permalink( $post->ID ) : '';
     }
