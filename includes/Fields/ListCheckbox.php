395229861c378d9e78a255d0de0f45f03af74221
gelther <lpw.sormano@gmail.com>
2016-04-10 13:20:02 +0200

    Code consistency changes for includes/Fields/ListCheckbox.php

--- includes/Fields/ListCheckbox.php
+++ includes/Fields/ListCheckbox.php
@@ -32,7 +32,7 @@ class NF_Fields_ListCheckbox extends NF_Abstracts_List
 
         $list = '';
         foreach( $field->get_setting( 'options' ) as $option ){
-            $checked  = ( in_array( $option[ 'value' ], $value ) ) ? "checked" : '';
+            $checked  = ( in_array( $option['value'], $value ) ) ? "checked" : '';
             $list    .= "<li><label><input type='checkbox' value='{$option[ 'value' ]}' name='fields[$id][]' $checked> {$option[ 'label' ]}</label></li>";
         }
 
