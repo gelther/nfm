12321c50051bfefd72a66887d2ae0d2edc5b5991
gelther <lpw.sormano@gmail.com>
2016-04-10 16:10:39 +0200

    Code consistency changes for includes/Admin/Menus/Preview.php

--- includes/Admin/Menus/Preview.php
+++ includes/Admin/Menus/Preview.php
@@ -8,13 +8,11 @@ final class NF_Admin_Menus_Preview extends NF_Abstracts_Submenu
 
     public $priority = 9001;
 
-    public function __construct()
-    {
+    public function __construct() {
         parent::__construct();
     }
 
-    public function display()
-    {
+    public function display() {
         Ninja_Forms()->template( 'admin-menu-preview.html.php' );
     }
 
