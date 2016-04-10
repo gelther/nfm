63c3f3da5c6b4855af98cfffbad0b631cb296a48
gelther <lpw.sormano@gmail.com>
2016-04-10 09:39:46 +0200

    Code consistency changes for includes/Fields/Textarea.php

--- includes/Fields/Textarea.php
+++ includes/Fields/Textarea.php
@@ -25,7 +25,7 @@ class NF_Fields_Textarea extends NF_Abstracts_Input
 
         $this->_nicename = __( 'Paragraph Text', 'ninja-forms' );
 
-        $this->_settings[ 'default' ][ 'type' ] = 'textarea';
+        $this->_settings[ 'default' ][ 'type' ]     = 'textarea';
         $this->_settings[ 'placeholder' ][ 'type' ] = 'textarea';
     }
 
