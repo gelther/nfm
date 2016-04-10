fc4132d602342017a51bb386614a14c585c8f96b
gelther <lpw.sormano@gmail.com>
2016-04-10 15:57:08 +0200

    Code consistency changes for includes/Admin/Metaboxes/AppendAForm.php

--- includes/Admin/Metaboxes/AppendAForm.php
+++ includes/Admin/Metaboxes/AppendAForm.php
@@ -18,7 +18,7 @@ final class NF_Admin_Metaboxes_AppendAForm extends NF_Abstracts_Metabox
         $post    = $GLOBALS[ 'post' ];
         $form_id = get_post_meta( $post->ID, 'ninja_forms_form', true );
 
-        if( ! $form_id ) return $content;
+        if ( ! $form_id ) return $content;
 
         return $content . "[ninja_forms id=$form_id]";
     }
