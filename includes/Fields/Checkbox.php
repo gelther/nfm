5c2ebde5c26ef822fd672735578d0e865bb24d8c
gelther <lpw.sormano@gmail.com>
2016-04-10 16:11:55 +0200

    Code consistency changes for includes/Fields/Checkbox.php

--- includes/Fields/Checkbox.php
+++ includes/Fields/Checkbox.php
@@ -23,8 +23,7 @@ class NF_Fields_Checkbox extends NF_Abstracts_Input
 
     protected $_settings_exclude = array( 'default', 'placeholder', 'input_limit_set' );
 
-    public function __construct()
-    {
+    public function __construct() {
         parent::__construct();
 
         $this->_nicename = __( 'Single Checkbox', 'ninja-forms' );
@@ -34,15 +33,13 @@ class NF_Fields_Checkbox extends NF_Abstracts_Input
         add_filter( 'ninja_forms_custom_columns', array( $this, 'custom_columns' ), 10, 2 );
     }
 
-    public function admin_form_element( $id, $value )
-    {
+    public function admin_form_element( $id, $value ) {
         $checked = ( $value ) ? 'checked' : '';
 
         return "<input type='checkbox' name='fields[$id]' id='' $checked>";
     }
 
-    public function custom_columns( $value, $field )
-    {
+    public function custom_columns( $value, $field ) {
         if ( 'checkbox' == $field->get_setting( 'type' ) ) {
             $value = ( $value ) ? __( 'checked', 'ninja-forms' ) : __( 'unchecked', 'ninja-forms' );
         }
