8495a42bf81d557c391429a4b463b3fe19b35173
gelther <lpw.sormano@gmail.com>
2016-04-10 09:38:16 +0200

    Code consistency changes for includes/Abstracts/FieldOptIn.php

--- includes/Abstracts/FieldOptIn.php
+++ includes/Abstracts/FieldOptIn.php
@@ -49,12 +49,12 @@ abstract class NF_Abstracts_FieldOptIn extends NF_Abstracts_Input
          * Add a refresh extra for the groups fieldset.
          */
         $this->_settings[ 'fieldset' ][ 'label' ] = __( 'Lists', 'ninja-forms' ) . ' <a href="#"><small>' . __( 'refresh', 'ninja-forms' ) . '</small></a>';
-        $this->_settings[ 'fieldset' ][ 'deps' ] = array( 'type' => 'multiple' );
+        $this->_settings[ 'fieldset' ][ 'deps' ]  = array( 'type' => 'multiple' );
 
         /*
          * Hide the 'type' and 'fieldset' ('groups') settings until they are ready for use.
          */
-        $this->_settings[ 'type' ][ 'group' ] = '';
+        $this->_settings[ 'type' ][ 'group' ]     = '';
         $this->_settings[ 'fieldset' ][ 'group' ] = '';
     }
 
