e35eb09a126fa080da1b0c6b3535e557adaff6aa
gelther <lpw.sormano@gmail.com>
2016-04-10 15:15:16 +0200

    Code consistency changes for includes/Actions/SuccessMessage.php

--- includes/Actions/SuccessMessage.php
+++ includes/Actions/SuccessMessage.php
@@ -46,7 +46,6 @@ final class NF_Actions_SuccessMessage extends NF_Abstracts_Action
     */
     public function save( $action_settings )
     {
-
     }
 
     public function process( $action_settings, $form_id, $data )
