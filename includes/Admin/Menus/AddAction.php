2c9a939b2e639b4d67f3dd4e6da38f14e0d91a42
gelther <lpw.sormano@gmail.com>
2016-04-10 16:10:17 +0200

    Code consistency changes for includes/Admin/Menus/AddAction.php

--- includes/Admin/Menus/AddAction.php
+++ includes/Admin/Menus/AddAction.php
@@ -8,13 +8,11 @@ final class NF_Admin_Menus_AddAction extends NF_Abstracts_Submenu
 
     public $priority = 9001;
 
-    public function __construct()
-    {
+    public function __construct() {
         parent::__construct();
     }
 
-    public function display()
-    {
+    public function display() {
         Ninja_Forms::template( 'admin-menu-add-action.html.php' );
         wp_enqueue_style( 'nf-builder', Ninja_Forms::$url . 'assets/css/builder.css' );
     }
