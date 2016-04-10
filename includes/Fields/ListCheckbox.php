6617b23eefae3c32e30c3cecfb4608706a79deeb
gelther <lpw.sormano@gmail.com>
2016-04-10 15:51:08 +0200

    Code consistency changes for includes/Fields/ListCheckbox.php

--- includes/Fields/ListCheckbox.php
+++ includes/Fields/ListCheckbox.php
@@ -31,7 +31,7 @@ class NF_Fields_ListCheckbox extends NF_Abstracts_List
         $field = Ninja_Forms()->form()->get_field( $id );
 
         $list = '';
-        foreach( $field->get_setting( 'options' ) as $option ){
+        foreach( $field->get_setting( 'options' ) as $option ) {
             $checked  = ( in_array( $option[ 'value' ], $value ) ) ? 'checked' : '';
             $list    .= "<li><label><input type='checkbox' value='{$option[ 'value' ]}' name='fields[$id][]' $checked> {$option[ 'label' ]}</label></li>";
         }
