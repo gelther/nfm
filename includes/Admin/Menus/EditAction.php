1992b3b0faef96059978a9b645d9661e48385a2c
gelther <lpw.sormano@gmail.com>
2016-04-10 16:10:25 +0200

    Code consistency changes for includes/Admin/Menus/EditAction.php

--- includes/Admin/Menus/EditAction.php
+++ includes/Admin/Menus/EditAction.php
@@ -8,13 +8,11 @@ final class NF_Admin_Menus_EditAction extends NF_Abstracts_Submenu
 
     public $priority = 9001;
 
-    public function __construct()
-    {
+    public function __construct() {
         parent::__construct();
     }
 
-    public function display()
-    {
+    public function display() {
         Ninja_Forms::template( 'admin-menu-edit-action.html.php' );
         wp_enqueue_style( 'nf-builder', Ninja_Forms::$url . 'assets/css/builder.css' );
     }
