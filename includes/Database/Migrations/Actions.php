6eaec47c8bad7b5976eb8cb798dcf53f733b3bd4
gelther <lpw.sormano@gmail.com>
2016-04-10 16:11:14 +0200

    Code consistency changes for includes/Database/Migrations/Actions.php

--- includes/Database/Migrations/Actions.php
+++ includes/Database/Migrations/Actions.php
@@ -2,16 +2,14 @@
 
 class NF_Database_Migrations_Actions extends NF_Abstracts_Migration
 {
-    public function __construct()
-    {
+    public function __construct() {
         parent::__construct(
             'nf3_actions',
             'nf_migration_create_table_actions'
         );
     }
 
-    public function run()
-    {
+    public function run() {
         $query = "CREATE TABLE IF NOT EXISTS $this->table_name (
             `id` int NOT NULL AUTO_INCREMENT,
             `title` longtext,
