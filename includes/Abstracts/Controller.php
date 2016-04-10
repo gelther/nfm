d09cf439882c71e92d5d11137024328d562bea90
gelther <lpw.sormano@gmail.com>
2016-04-10 12:44:57 +0200

    Code consistency changes for includes/Abstracts/Controller.php

--- includes/Abstracts/Controller.php
+++ includes/Abstracts/Controller.php
@@ -16,7 +16,7 @@ abstract class NF_Abstracts_Controller
      */
     protected $_data = array();
 
-    /*
+    /**
      * PUBLIC METHODS
      */
 
@@ -29,7 +29,7 @@ abstract class NF_Abstracts_Controller
     }
 
 
-    /*
+    /**
      * PROTECTED METHODS
      */
 
