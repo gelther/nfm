b09a57908a6a9c89f3b4f3aa4e9678bb3104100b
gelther <lpw.sormano@gmail.com>
2016-04-10 14:52:10 +0200

    Code consistency changes for includes/AJAX/Controllers/Uploads.php

--- includes/AJAX/Controllers/Uploads.php
+++ includes/AJAX/Controllers/Uploads.php
@@ -50,7 +50,7 @@ class NF_AJAX_Controllers_Uploads extends NF_Abstracts_Controller
 
                 $new_tmp_name = $this->i_like_clean_slugs_and_i_cannot_lie( $file[ 'name' ] );
 
-                $file_path = $upload_dir[ 'basedir' ] . "/" . $new_tmp_name;
+                $file_path = $upload_dir[ 'basedir' ] . '/' . $new_tmp_name;
 
                 move_uploaded_file( $file[ 'tmp_name' ], $file_path );
 
@@ -86,29 +86,29 @@ class NF_AJAX_Controllers_Uploads extends NF_Abstracts_Controller
     {
         switch ( $code ) {
             case UPLOAD_ERR_INI_SIZE:
-                $message = "The uploaded file exceeds the upload_max_filesize directive in php.ini";
+                $message = 'The uploaded file exceeds the upload_max_filesize directive in php.ini';
                 break;
             case UPLOAD_ERR_FORM_SIZE:
-                $message = "The uploaded file exceeds the MAX_FILE_SIZE directive that was specified in the HTML form";
+                $message = 'The uploaded file exceeds the MAX_FILE_SIZE directive that was specified in the HTML form';
                 break;
             case UPLOAD_ERR_PARTIAL:
-                $message = "The uploaded file was only partially uploaded";
+                $message = 'The uploaded file was only partially uploaded';
                 break;
             case UPLOAD_ERR_NO_FILE:
-                $message = "No file was uploaded";
+                $message = 'No file was uploaded';
                 break;
             case UPLOAD_ERR_NO_TMP_DIR:
-                $message = "Missing a temporary folder";
+                $message = 'Missing a temporary folder';
                 break;
             case UPLOAD_ERR_CANT_WRITE:
-                $message = "Failed to write file to disk";
+                $message = 'Failed to write file to disk';
                 break;
             case UPLOAD_ERR_EXTENSION:
-                $message = "File upload stopped by extension";
+                $message = 'File upload stopped by extension';
                 break;
 
             default:
-                $message = "Unknown upload error";
+                $message = 'Unknown upload error';
                 break;
         }
         return $message;
