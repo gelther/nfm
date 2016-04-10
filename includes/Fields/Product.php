849ebd5038a0f6239e67f820281c9d0c3aa1fada
gelther <lpw.sormano@gmail.com>
2016-04-10 17:56:57 +0200

    Code consistency changes for includes/Fields/Product.php

--- includes/Fields/Product.php
+++ includes/Fields/Product.php
@@ -36,39 +36,39 @@ class NF_Fields_Product extends NF_Abstracts_Input
     public function process( $product, $data ) {
         $related = array();
 
-        foreach ( $data[ 'fields' ] as $key => $field ) {
+        foreach ( $data['fields'] as $key => $field ) {
 
-            if ( ! in_array( $field[ 'type' ], $this->processing_fields ) ) continue;
+            if ( ! in_array( $field['type'], $this->processing_fields ) ) continue;
 
-            $type = $field[ 'type' ];
+            $type = $field['type'];
 
-            if ( ! isset( $field[ 'product_assignment' ] ) ) continue;
+            if ( ! isset( $field['product_assignment'] ) ) continue;
 
-            if ( $product[ 'id' ] != $field[ 'product_assignment' ] ) continue;
+            if ( $product['id'] != $field['product_assignment'] ) continue;
 
-            $related[ $type ] = &$data[ 'fields' ][ $key ]; // Assign by reference
+            $related[ $type ] = &$data['fields'][ $key ]; // Assign by reference
         }
 
-        $total = floatval( $product[ 'product_price' ] );
+        $total = floatval( $product['product_price'] );
 
-        if ( isset( $related[ 'quantity' ][ 'value' ] ) && $related[ 'quantity' ][ 'value' ] ) {
-            $total = $total * $related[ 'quantity' ][ 'value' ];
-        } elseif( $product[ 'product_use_quantity' ] && $product[ 'value' ] ) {
-            $total = $total * $product[ 'value' ];
+        if ( isset( $related['quantity']['value'] ) && $related['quantity']['value'] ) {
+            $total = $total * $related['quantity']['value'];
+        } elseif( $product['product_use_quantity'] && $product['value'] ) {
+            $total = $total * $product['value'];
         }
 
-        if ( isset( $related[ 'modifier' ] ) ) {
+        if ( isset( $related['modifier'] ) ) {
             //TODO: Handle multiple modifiers.
         }
 
-        $data[ 'product_totals' ][] = number_format( $total, 2 );
+        $data['product_totals'][] = number_format( $total, 2 );
 
         return $data;
     }
 
     public function merge_tag_value( $value, $field ) {
-        if ( isset( $field[ 'product_price' ] ) ) {
-            $value = $field[ 'product_price' ];
+        if ( isset( $field['product_price'] ) ) {
+            $value = $field['product_price'];
         }
 
         return $value;
