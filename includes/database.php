c8d5d153108ec1ebb09ad7c7d28ee6e3d455d9bb
gelther <lpw.sormano@gmail.com>
2016-04-10 16:13:05 +0200

    Code consistency changes for includes/database.php

--- includes/database.php
+++ includes/database.php
@@ -8,6 +8,6 @@
 | Included for backwards compatibility with Visual Composer.
 |
  */
-function ninja_forms_get_all_forms(){
+function ninja_forms_get_all_forms() {
     return Ninja_Forms()->form()->get_forms();
 }
