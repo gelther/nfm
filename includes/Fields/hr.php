36ecc1be00c4d748bbcea3fab72457e80b6db106
gelther <lpw.sormano@gmail.com>
2016-04-10 13:33:08 +0200

    Code consistency changes for includes/Fields/hr.php

--- includes/Fields/hr.php
+++ includes/Fields/hr.php
@@ -28,12 +28,12 @@ class NF_Fields_Hr extends NF_Abstracts_Input
         $this->_nicename = __( 'Divider', 'ninja-forms' );
         add_filter( 'nf_sub_hidden_field_types', array( $this, 'hide_field_type' ) );
 
-        unset( $this->_settings[ 'classes' ][ 'settings' ][ 'wrapper '] );
+        unset( $this->_settings[ 'classes' ][ 'settings' ][ 'wrapper ' ] );
     }
 
     function hide_field_type( $field_types )
     {
-        $field_types[] = $this->_name;
+        $field_types[  ] = $this->_name;
 
         return $field_types;
     }
