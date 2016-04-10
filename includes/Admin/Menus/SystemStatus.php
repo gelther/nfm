c0845f28334b27b786bf26d878585c5b65409fbd
gelther <lpw.sormano@gmail.com>
2016-04-10 16:10:45 +0200

    Code consistency changes for includes/Admin/Menus/SystemStatus.php

--- includes/Admin/Menus/SystemStatus.php
+++ includes/Admin/Menus/SystemStatus.php
@@ -8,13 +8,11 @@ final class NF_Admin_Menus_SystemStatus extends NF_Abstracts_Submenu
 
     public $priority = 12;
 
-    public function __construct()
-    {
+    public function __construct() {
         parent::__construct();
     }
 
-    public function display()
-    {
+    public function display() {
         /** @global wpdb $wpdb */
         global $wpdb;
 
