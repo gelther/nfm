c4c53a8dfbbfb7b7e799e56ff971eb6533f6fdb8
gelther <lpw.sormano@gmail.com>
2016-04-10 14:36:55 +0200

    Code consistency changes for includes/Helper.php

--- includes/Helper.php
+++ includes/Helper.php
@@ -163,7 +163,7 @@ final class WPN_Helper
                 switch ( gettype( $workArray[$i] ) ) {
                     // Manually set some strings
                     case "NULL":     $_spFormat = ''; break;
-                    case "boolean":  $_spFormat = ($workArray[$i] == true) ? 'true': 'false'; break;
+                    case "boolean":  $_spFormat = ($workArray[$i] == TRUE) ? 'true': 'false'; break;
                     // Make sure sprintf has a good datatype to work with
                     case "integer":  $_spFormat = '%i'; break;
                     case "double":   $_spFormat = '%0.2f'; break;
