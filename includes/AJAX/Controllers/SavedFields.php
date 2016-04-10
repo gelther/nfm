6c9b911573929590e6ae7c0776576c40e680af65
gelther <lpw.sormano@gmail.com>
2016-04-10 13:31:07 +0200

    Code consistency changes for includes/AJAX/Controllers/SavedFields.php

--- includes/AJAX/Controllers/SavedFields.php
+++ includes/AJAX/Controllers/SavedFields.php
@@ -14,7 +14,7 @@ class NF_AJAX_Controllers_SavedFields extends NF_Abstracts_Controller
         check_ajax_referer( 'ninja_forms_ajax_nonce', 'security' );
 
         if( ! isset( $_POST[ 'field' ] ) ){
-            $this->_errors[] = 'Field Not Found';
+            $this->_errors[  ] = 'Field Not Found';
             $this->_respond();
         }
 
@@ -35,7 +35,7 @@ class NF_AJAX_Controllers_SavedFields extends NF_Abstracts_Controller
         check_ajax_referer( 'ninja_forms_ajax_nonce', 'security' );
 
         if( ! isset( $_POST[ 'field' ] ) ){
-            $this->_errors[] = 'Field Not Found';
+            $this->_errors[  ] = 'Field Not Found';
             $this->_respond();
         }
 
@@ -47,7 +47,7 @@ class NF_AJAX_Controllers_SavedFields extends NF_Abstracts_Controller
         check_ajax_referer( 'ninja_forms_ajax_nonce', 'security' );
 
         if( ! isset( $_POST[ 'field' ] ) ){
-            $this->_errors[] = 'Field Not Found';
+            $this->_errors[  ] = 'Field Not Found';
             $this->_respond();
         }
 
